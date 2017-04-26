1409107aa394b3dc14e97df665fe251207e7f03c
Attila Gulyas-Kovacs <attila.gulyas.kovacs@gmail.com>
2017-04-26 17:57:18 -0400

    Refined hostspecific.sh

--- hostspecific.sh
+++ hostspecific.sh
@@ -9,16 +9,16 @@ case `hostname` in
         export BOWTIE2_INDEX=$HOME/GRCh37/dna/bowtie2/Homo_sapiens.GRCh37.75.dna.primary_assembly
         ;;
     minerva*)
-        echo "I am $USER on `hostname`"
-        # revert the annoying hack meant to support new ssl despite Minerva's old linux kernel
-        module unload openssl
-        module load R git
         PATH="$PATH:/hpc/users/$USER/chess01c/local/bin:/hpc/users/$USER/chess01c/opt/bin"
+        # commands to execute
+        module load R git vim/8.0
+        module unload openssl # no effect; revert the annoying hack meant to support new ssl despite Minerva's old linux kernel
+        # special variables, mostly for bioinformatics
         export BWA_INDEX=/sc/orga/projects/chessa01c/refgenome/GRCh37/dna/bwa/Homo_sapiens.GRCh37.75.dna.primary_assembly
+        export MODULE_BWA=bwa#/0.7.15
         export BOWTIE2_INDEX=/sc/orga/projects/chessa01c/refgenome/GRCh37/dna/bowtie2/Homo_sapiens.GRCh37.75.dna.primary_assembly
+        export MODULE_BOWTIE2=bowtie2#/2.2.8
         export BSUB_STDOUT=/hpc/users/$USER/.bsub.stdout
         export BSUB_STDERR=/hpc/users/$USER/.bsub.stderr
-        export MODULE_BWA=bwa/0.7.15
-        export MODULE_BOWTIE2=bowtie2/2.2.8
         ;;
 esac
